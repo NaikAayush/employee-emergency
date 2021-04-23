8dcbcb4a10a347575d368af84757121fdc2e46d5
Samyak S Sarnayak <samyak201@gmail.com>
2021-04-23 23:50:42 +0530

    switch to FastAPI

--- command-center/api/app.py
+++ command-center/api/app.py
@@ -1,15 +1,37 @@
-from quart import Quart, request
-from processing.map import process_map
+import io
+
+from fastapi import FastAPI, UploadFile, File
+from starlette.responses import StreamingResponse
+
+from PIL import Image
 import cv2
+import numpy as np
 
-app = Quart(__name__)
+from processing.map import process_map
 
-@app.route("/")
+app = FastAPI()
+
+
+@app.get("/")
 async def index():
-    return "no. dont."
+    return {"message": "no. dont."}
 
-@app.route("/map/processImage", methods=["POST"])
-async def processImage():
-    file_ = request.files["image"]
-    file_.stream
-    return "TODO: process image here"
+
+@app.post("/map/processImage")
+async def processImage(file: UploadFile = File(...)):
+    pil_img = Image.open(file.file)
+
+    img = cv2.cvtColor(np.array(pil_img), cv2.COLOR_RGB2BGR)
+
+    better_img = process_map(img)
+
+    # return {
+    #     "filename": file.filename,
+    #     "width": pil_img.width,
+    #     "height": pil_img.height
+    # }
+
+    _, img_png = cv2.imencode(".png", better_img)
+
+    return StreamingResponse(io.BytesIO(img_png.tobytes()), media_type="image/png")
+
