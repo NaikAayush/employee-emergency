6d8452634bc9a262d2c96279d2a1b3640d4dd215
Samyak S Sarnayak <samyak201@gmail.com>
2021-04-24 23:08:51 +0530

    cc - move to Fabric JS from canvas

--- command-center/command-center-client/src/app/views/upload/upload/upload.component.ts
+++ command-center/command-center-client/src/app/views/upload/upload/upload.component.ts
@@ -1,5 +1,6 @@
 import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {ApiService} from 'src/app/services/api.service';
+import {fabric} from 'fabric';
 
 class Point {
     x: number;
@@ -11,6 +12,11 @@ class Point {
     }
 }
 
+interface ChoiceInfo {
+    iconEl: HTMLImageElement,
+    iconSrc: string
+}
+
 @Component({
     selector: 'app-upload',
     templateUrl: './upload.component.html',
@@ -20,74 +26,96 @@ export class UploadComponent implements OnInit {
 
     title = 'command-center';
 
-    @ViewChild('mapCanvas', {static: false})
-    private mapCanvas!: ElementRef<HTMLCanvasElement>;
+    private canvas!: fabric.Canvas;
 
-    private context!: CanvasRenderingContext2D;
-    private rect!: DOMRect;
+    // private contextF!: CanvasRenderingContext2D;
     private origImg!: HTMLImageElement;
     private mapImg!: HTMLImageElement;
-    private markerLocs: any = {};
+    // private markerLocs: any = {};
     public markerChoice: string = "exit";
 
     // icons
     private exitIcon!: HTMLImageElement;
+    private entryIcon!: HTMLImageElement;
+    private beaconIcon!: HTMLImageElement;
+
+    // choices, so many choices
+    private choicesInfo: Record<string, ChoiceInfo> = {
+        "exit": {
+            "iconEl": this.exitIcon,
+            "iconSrc": "assets/img/exit.svg"
+        },
+        "entry": {
+            "iconEl": this.entryIcon,
+            "iconSrc": "assets/img/entry.svg"
+        },
+        "beacon": {
+            "iconEl": this.beaconIcon,
+            "iconSrc": "assets/img/beacon.svg"
+        }
+    };
+    // for use in ngFor ig
+    private choices = ["exit", "entry", "beacon"];
 
     constructor(public api: ApiService) {}
 
     ngOnInit(): void {
+        this.canvas = new fabric.Canvas("mapFabricCanvas");
+        this.canvas.selection = false;
+        this.canvas.on("mouse:down", (e: fabric.IEvent) => {
+            if (e.pointer === undefined) {
+                console.log("aaaaa undefined");
+            } else {
+                console.log(e.pointer.x, e.pointer.y, this.markerChoice);
+
+                const iconSize = 20;
+                let x = e.pointer.x - iconSize / 2;
+                let y = e.pointer.y - iconSize / 2;
+                let intersects = false;
+                let intObject: fabric.Object | null = null;
+
+                this.canvas.getObjects('image').some(obj => {
+                    let inter = obj.intersectsWithRect(
+                        new fabric.Point(x, y),
+                        new fabric.Point(x + iconSize, y + iconSize)
+                    );
+                    if (inter) {
+                        intersects = true;
+                        intObject = obj;
+                        return true;
+                    }
+                    return false;
+                });
+
+                if (intersects && intObject !== null) {
+                    this.canvas.remove(intObject);
+                } else {
+                    let iconImg = new fabric.Image(this.choicesInfo[this.markerChoice].iconEl, {
+                        left: x,
+                        top: y,
+                        selectable: false,
+                        width: iconSize,
+                        height: iconSize
+                    });
+                    this.canvas.add(iconImg);
+                }
+            }
+
+        });
     }
 
     ngAfterViewInit(): void {
-        let context = this.mapCanvas.nativeElement.getContext('2d');
-        if (context === null) {
-            console.log("Error in canvas initialization. AAAAAA");
-        } else {
-            this.context = context;
-        }
+        this.canvas.height = 0;
+        this.canvas.width = 0;
 
         this.exitIcon = new Image();
         this.exitIcon.src = "assets/img/exit.svg";
-    }
 
-    private getMousePos(evt: MouseEvent): Point {
-        let rect = this.rect;
-        let canvas = this.mapCanvas.nativeElement;
-        if (rect === undefined) {
-            return new Point(0, 0);
-        } else {
-            // let x = (evt.clientX - rect.left) / (rect.right - rect.left) * canvas.width;
-            // let y = (evt.clientY - rect.top) / (rect.bottom - rect.top) * canvas.height;
-            // return new Point(x, y);
-
-            const rect = canvas.getBoundingClientRect(), // abs. size of element
-                scaleX = canvas.width / rect.width,    // relationship bitmap vs. element for X
-                scaleY = canvas.height / rect.height;  // relationship bitmap vs. element for Y
-            const px = (evt.clientX - rect.left) * scaleX;
-            const py = (evt.clientY - rect.top) * scaleY;
-
-            const matrix = this.context.getTransform();
-            const imatrix = matrix.invertSelf();
-            const x = px * imatrix.a + py * imatrix.c + imatrix.e;
-            const y = px * imatrix.b + py * imatrix.d + imatrix.f;
-
-            return new Point(x, y);
-        }
-    }
-
-    handleMouseClick(evt: MouseEvent): void {
-        let pos = this.getMousePos(evt);
-
-        console.log(this.markerLocs);
-        if (this.markerLocs[this.markerChoice] === undefined) {
-            this.markerLocs[this.markerChoice] = [];
-        }
-        this.markerLocs[this.markerChoice].push(pos);
-
-        console.log(pos.x, pos.y, this.markerChoice);
-
-        const iconSize = 20;
-        this.context.drawImage(this.exitIcon, pos.x - iconSize / 2, pos.y - iconSize / 2, iconSize, iconSize);
+        this.choices.forEach((choice: string) => {
+            let choiceInfo = this.choicesInfo[choice];
+            choiceInfo.iconEl = new Image();
+            choiceInfo.iconEl.src = choiceInfo.iconSrc;
+        });
     }
 
     handleFileInput(target: any) {
@@ -105,11 +133,20 @@ export class UploadComponent implements OnInit {
                 orig_img.src = "data:image/jpg;base64," + res.orig_img;
 
                 orig_img.onload = () => {
-                    this.mapCanvas.nativeElement.width = orig_img.width;
-                    this.mapCanvas.nativeElement.height = orig_img.height;
-                    this.context.drawImage(orig_img, 0, 0);
+                    this.canvas.setWidth(orig_img.width);
+                    // this.canvas.height = orig_img.height;
+                    this.canvas.setHeight(orig_img.height);
+                    // this.canvas.width = orig_img.width;
+                    this.canvas.setDimensions({
+                        width: '100%',
+                        height: ''
+                    }, {
+                        cssOnly: true
+                    });
+                    this.canvas.hoverCursor = 'pointer';
 
-                    this.rect = this.mapCanvas.nativeElement.getBoundingClientRect();
+                    let orig_img_f = new fabric.Image(orig_img, {left: 0, top: 0, lockMovementX: true, lockMovementY: true, lockScalingX: true, selectable: false});
+                    this.canvas.add(orig_img_f);
                 }
 
                 let map_img = new Image();
@@ -123,9 +160,6 @@ export class UploadComponent implements OnInit {
                 console.log("error in upload image", err)
             }
         )
-        // TODO: upload to quart server
-        // TODO: show in canvas
-        // TODO: put markers on canvas
         // TODO: aaaaaaaaaaaaaa
     }
 
