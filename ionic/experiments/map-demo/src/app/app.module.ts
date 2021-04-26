b34f29a9f9d7b058f0ec3e0d5d50aa0ac0ce74ce
Samyak S Sarnayak <samyak201@gmail.com>
2021-04-26 23:25:55 +0530

    map demo - draw whole map with markers from dbs

--- ionic/experiments/map-demo/src/app/app.module.ts
+++ ionic/experiments/map-demo/src/app/app.module.ts
@@ -8,10 +8,27 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { FormsModule } from '@angular/forms';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { environment } from 'src/environments/environment';
+import { HttpClientModule } from '@angular/common/http';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    AngularFireStorageModule,
+    HttpClientModule,
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- ionic/experiments/map-demo/src/app/views/upload/upload/upload.component.ts
+++ ionic/experiments/map-demo/src/app/views/upload/upload/upload.component.ts
@@ -1,10 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { fabric } from 'fabric';
 import {
   AngularFirestore,
   AngularFirestoreDocument,
 } from '@angular/fire/firestore';
 import { AngularFireStorage } from '@angular/fire/storage';
+import { ApiService } from 'src/app/services/api.service';
+
+interface MarkerInfo {
+  name: string;
+  top: number;
+  left: number;
+  width: number;
+  height: number;
+}
+
+interface MapInfo {
+  markers: MarkerInfo[];
+}
+
+interface ChoiceInfo {
+  iconEl: HTMLImageElement;
+  iconSrc: string;
+}
 
 @Component({
   selector: 'app-upload',
@@ -12,7 +30,131 @@ import { AngularFireStorage } from '@angular/fire/storage';
   styleUrls: ['./upload.component.scss'],
 })
 export class UploadComponent implements OnInit {
-  constructor() {}
+  uuid: string;
 
-  ngOnInit() {}
+  private mapDoc: AngularFirestoreDocument<MapInfo>;
+  private canvas: fabric.Canvas;
+
+  // icons
+  private exitIcon!: HTMLImageElement;
+  private entryIcon!: HTMLImageElement;
+  private beaconIcon!: HTMLImageElement;
+
+  // choices, so many choices
+  private choicesInfo: Record<string, ChoiceInfo> = {
+    exit: {
+      iconEl: this.exitIcon,
+      iconSrc: 'assets/img/exit.svg',
+    },
+    entry: {
+      iconEl: this.entryIcon,
+      iconSrc: 'assets/img/entry.svg',
+    },
+    beacon: {
+      iconEl: this.beaconIcon,
+      iconSrc: 'assets/img/beacon.svg',
+    },
+  };
+  // for use in ngFor ig
+  private choices = ['exit', 'entry', 'beacon'];
+
+  constructor(
+    private api: ApiService,
+    private firestore: AngularFirestore,
+    private storage: AngularFireStorage
+  ) {
+    this.mapDoc = firestore.doc<MapInfo>('map/unknown');
+  }
+
+  ngOnInit() {
+    this.canvas = new fabric.Canvas('mapFabricCanvas');
+    this.canvas.selection = false;
+
+    this.canvas.height = 0;
+    this.canvas.width = 0;
+
+    this.choices.forEach((choice: string) => {
+      let choiceInfo = this.choicesInfo[choice];
+      choiceInfo.iconEl = new Image();
+      choiceInfo.iconEl.src = choiceInfo.iconSrc;
+    });
+  }
+
+  handleSubmitClick(uuid: string) {
+    this.uuid = uuid;
+    this.mapDoc = this.firestore.doc<MapInfo>('map/' + uuid);
+    console.log('hiii', uuid);
+
+    const imgRef = this.storage.ref('map/' + this.uuid + '/orig');
+    imgRef
+      .getDownloadURL()
+      .toPromise()
+      .then((res) => {
+        console.log(res);
+
+        let orig_img = new Image();
+        orig_img.src = res;
+
+        orig_img.onload = () => {
+          // this.canvas.setDimensions({
+          //   width: orig_img.width,
+          //   height: orig_img.height,
+          // });
+          this.canvas.setWidth(orig_img.width);
+          // this.canvas.height = orig_img.height;
+          this.canvas.setHeight(orig_img.height);
+          // this.canvas.width = orig_img.width;
+          this.canvas.setDimensions(
+            {
+              width: '100%',
+              height: '',
+            },
+            {
+              cssOnly: true,
+            }
+          );
+
+          const height = this.canvas.getHeight();
+          console.log('height is ', height);
+          this.canvas.hoverCursor = 'auto';
+
+          let orig_img_f = new fabric.Image(orig_img, {
+            left: 0,
+            top: 0,
+            lockMovementX: true,
+            lockMovementY: true,
+            lockScalingX: true,
+            selectable: false,
+          });
+          this.canvas.add(orig_img_f);
+
+          this.addMarkers();
+        };
+      })
+      .catch((err) => {
+        console.log('error go brr', err);
+      });
+  }
+
+  private addMarkers() {
+    this.mapDoc
+      .get()
+      .toPromise()
+      .then((res) => {
+        const mapData = res.data();
+        console.log(mapData);
+
+        for (let marker of mapData.markers) {
+          let iconImg = new fabric.Image(this.choicesInfo[marker.name].iconEl, {
+            left: marker.left,
+            top: marker.top,
+            selectable: false,
+            width: marker.width,
+            height: marker.height,
+          });
+          iconImg.data = { name: marker.name };
+          this.canvas.add(iconImg);
+        }
+      });
+  }
 }
