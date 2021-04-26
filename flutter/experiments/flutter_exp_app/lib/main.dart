40a6e701b056aa9f45bc6b51e38878a96d9df2c7
NaikAayush <ayush@pesu.pes.edu>
2021-04-26 19:08:37 +0530

    refactor folders

---
+++ flutter/experiments/flutter_exp_app/lib/main.dart
@@ -0,0 +1,153 @@
+import 'dart:async';
+import 'dart:io' show Platform;
+import 'dart:convert';
+
+import 'package:beacons_plugin/beacons_plugin.dart';
+import 'package:flutter/material.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String _beaconResult = 'Not Scanned Yet.';
+  int _nrMessaggesReceived = 0;
+  var isRunning = false;
+
+  final StreamController<String> beaconEventsController =
+      StreamController<String>.broadcast();
+
+  @override
+  void initState() {
+    super.initState();
+    initPlatformState();
+  }
+
+  @override
+  void dispose() {
+    beaconEventsController.close();
+    super.dispose();
+  }
+
+  // Platform messages are asynchronous, so we initialize in an async method.
+  Future<void> initPlatformState() async {
+    if (Platform.isAndroid) {
+      //Prominent disclosure
+      await BeaconsPlugin.setDisclosureDialogMessage(
+          title: "Need Location Permission",
+          message: "This app collects location data to work with beacons.");
+
+      //Only in case, you want the dialog to be shown again. By Default, dialog will never be shown if permissions are granted.
+      //await BeaconsPlugin.clearDisclosureDialogShowFlag(false);
+    }
+
+    BeaconsPlugin.listenToBeacons(beaconEventsController);
+
+    await BeaconsPlugin.addRegion(
+        "BeaconType1", "909c3cf9-fc5c-4841-b695-380958a51a5a");
+    await BeaconsPlugin.addRegion(
+        "BeaconType2", "6a84c716-0f2a-1ce9-f210-6a63bd873dd9");
+
+    beaconEventsController.stream.listen(
+        (data) {
+          if (data.isNotEmpty) {
+            setState(() {
+              _beaconResult = data;
+              _nrMessaggesReceived++;
+            });
+            // print("Beacons DataReceived: " + data);
+            Data data_1 = Data.fromJson(jsonDecode(data));
+            print(data_1.minor);
+          }
+        },
+        onDone: () {},
+        onError: (error) {
+          print("Error: $error");
+        });
+
+    //Send 'true' to run in background
+    await BeaconsPlugin.runInBackground(true);
+
+    if (Platform.isAndroid) {
+      BeaconsPlugin.channel.setMethodCallHandler((call) async {
+        if (call.method == 'scannerReady') {
+          await BeaconsPlugin.startMonitoring();
+          setState(() {
+            isRunning = true;
+          });
+        }
+      });
+    } else if (Platform.isIOS) {
+      await BeaconsPlugin.startMonitoring();
+      setState(() {
+        isRunning = true;
+      });
+    }
+
+    if (!mounted) return;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Monitoring Beacons'),
+        ),
+        body: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text('$_beaconResult'),
+              Padding(
+                padding: EdgeInsets.all(10.0),
+              ),
+              Text('$_nrMessaggesReceived'),
+              SizedBox(
+                height: 20.0,
+              ),
+              ElevatedButton(
+                onPressed: () async {
+                  if (isRunning) {
+                    await BeaconsPlugin.stopMonitoring();
+                  } else {
+                    initPlatformState();
+                    await BeaconsPlugin.startMonitoring();
+                  }
+                  setState(() {
+                    isRunning = !isRunning;
+                  });
+                },
+                child: Text(isRunning ? 'Stop Scanning' : 'Start Scanning',
+                    style: TextStyle(fontSize: 20)),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Data {
+  String minor;
+  Data(this.minor);
+  factory Data.fromJson(dynamic json) {
+    return Data(json['minor'] as String);
+  }
+
+  @override
+  String toString() {
+    return '{${this.minor} }';
+  }
+}
