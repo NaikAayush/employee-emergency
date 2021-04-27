7b3f42bacd049261f54db091e2e4d972d9bb5a0b
Samyak S Sarnayak <samyak201@gmail.com>
2021-04-27 10:45:04 +0530

    nicer img upload and call processMarkers

--- misc/command-center-client/src/app/services/api.service.ts
+++ misc/command-center-client/src/app/services/api.service.ts
@@ -1,22 +1,27 @@
-import {HttpClient} from '@angular/common/http';
-import {Message} from '@angular/compiler/src/i18n/i18n_ast';
-import {Injectable} from '@angular/core';
-import {environment} from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Message } from '@angular/compiler/src/i18n/i18n_ast';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiService {
+  constructor(private http: HttpClient) {}
 
-    constructor(private http: HttpClient) {}
+  async get(route: String, params?: Record<string, string>) {
+    let url = `${environment.apiUrl}${route}`;
 
-    async get(route: String) {
-        return await this.http
-            .get<Message>(`${environment.apiUrl}${route}`)
-            .toPromise();
+    if (params) {
+      url += '?' + new URLSearchParams(params);
     }
 
-    async post(route: String, data: any) {
-        return await this.http.post(`${environment.apiUrl}${route}`, data, {responseType: "json"}).toPromise();
-    }
+    return await this.http.get<Message>(url).toPromise();
+  }
+
+  async post(route: String, data: any) {
+    return await this.http
+      .post(`${environment.apiUrl}${route}`, data, { responseType: 'json' })
+      .toPromise();
+  }
 }
