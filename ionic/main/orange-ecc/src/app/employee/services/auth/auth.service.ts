8f15157d6145459f40c248d6e44a2d2264873bc8
NaikAayush <ayush@pesu.pes.edu>
2021-04-30 00:21:09 +0530

    implemented employee sign in auth

---
+++ ionic/main/orange-ecc/src/app/employee/services/auth/auth.service.ts
@@ -0,0 +1,60 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
+import { Router } from '@angular/router';
+import { Observable, of } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  user$: Observable<any>;
+  constructor(private afAuth: AngularFireAuth, private afs: AngularFirestore, private router: Router) {
+    this.user$ = this.afAuth.authState.pipe(
+      switchMap(user => {
+        // Logged in
+        if (user) {
+          return this.afs.doc<any>(`users/${user.uid}`).valueChanges();
+        } else {
+          // Logged out
+          return of(null);
+        }
+      })
+    )
+  }
+
+  async signInWithEmail(email, password) {
+    const credential = await this.afAuth.signInWithEmailAndPassword(email, password);
+    return this.updateUserData(credential.user);
+  }
+
+  async signUpWithEmail(email, password) {
+    const credential = await this.afAuth.createUserWithEmailAndPassword(email, password);
+    return this.updateUserData(credential.user);
+  }
+
+  async emailPasswordReset(email) {
+    await this.afAuth.sendPasswordResetEmail(email);
+    const serverMessage = 'Check your email';
+  }
+
+  private updateUserData(user) {
+    // Sets user data to firestore on login
+    const userRef: AngularFirestoreDocument<any> = this.afs.doc(`users/${user.uid}`);
+
+    const data = {
+      uid: user.uid,
+      email: user.email,
+    }
+
+    return userRef.set(data, { merge: true })
+
+  }
+
+  async signOut() {
+    await this.afAuth.signOut();
+    this.router.navigate(['/']);
+  }
+
+}
