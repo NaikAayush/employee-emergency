8bdf2ac77ef4a0fb0704157df5db344ab73a91cd
NaikAayush <ayush@pesu.pes.edu>
2021-05-12 16:56:09 +0530

    WiFi RSSI

---
+++ ionic/experiments/wifi-rssi/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+import { WifiWizard2 } from '@ionic-native/wifi-wizard-2/ngx';
+
+@NgModule({
+  declarations: [AppComponent],
+  entryComponents: [],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    WifiWizard2,
+  ],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
