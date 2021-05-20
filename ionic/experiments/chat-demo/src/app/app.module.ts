acbb68915ee4f9e42a8fe30786803291a7a725d5
Samyak S Sarnayak <samyak201@gmail.com>
2021-05-21 01:08:17 +0530

    add not working ngFor thing

--- ionic/experiments/chat-demo/src/app/app.module.ts
+++ ionic/experiments/chat-demo/src/app/app.module.ts
@@ -6,12 +6,16 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { AngularFireModule } from '@angular/fire';
+import { environment } from 'src/environments/environment';
+import { CommonModule } from '@angular/common';
+import { ChatComponent } from './chat/chat/chat.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ChatComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, AngularFireModule.initializeApp(environment.firebase), CommonModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
