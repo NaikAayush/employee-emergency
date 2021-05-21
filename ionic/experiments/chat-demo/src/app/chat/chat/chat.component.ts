1c50e45353b9f5d0a5b4a4e4074a36845f5d6080
Samyak S Sarnayak <samyak201@gmail.com>
2021-05-21 17:19:29 +0530

    move chat to a service

--- ionic/experiments/chat-demo/src/app/chat/chat/chat.component.ts
+++ ionic/experiments/chat-demo/src/app/chat/chat/chat.component.ts
@@ -1,18 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { AngularFirestore } from '@angular/fire/firestore';
-import { AngularFireDatabase, AngularFireList } from '@angular/fire/database';
-import { Observable } from 'rxjs';
+import { ChatService } from 'src/app/service/chat.service';
 
 // TODO:
 //  1. demo with fixed sender and receiver id
 //  2. get nearest ERT automatically
 //  3. chat with command center
 
-class Message {
-  content: string;
-  sender: string;
-  senderId: string;
-}
 
 @Component({
   selector: 'app-chat',
@@ -20,37 +13,16 @@ class Message {
   styleUrls: ['./chat.component.scss'],
 })
 export class ChatComponent implements OnInit {
-  public senderUid: string;
-  public receiverUid: string;
-  public senderName: string;
-  public messages: Message[];
-
-  private messagesRef: AngularFireList<Message>;
-  private messageObs: Observable<Message[]>;
-
   constructor(
-    public firestore: AngularFirestore,
-    public db: AngularFireDatabase
+    public chat: ChatService
   ) {}
 
   ngOnInit() { }
 
   public handleSubmitClick(senderUid_: string, receiverUid_: string) {
     console.log('submit clicked', senderUid_, receiverUid_);
-    this.senderUid = senderUid_;
-    this.receiverUid = receiverUid_;
-    this.senderName = senderUid_;
 
-    if (this.senderUid < this.receiverUid) {
-    this.messagesRef = this.db.list(
-      `messages/${this.senderUid}_${this.receiverUid}`
-    );
-    } else {
-    this.messagesRef = this.db.list(
-      `messages/${this.receiverUid}_${this.senderUid}`
-    );
-    }
-    this.messageObs = this.messagesRef.valueChanges();
+    this.chat.init(senderUid_, receiverUid_, senderUid_);
 
     // this.messageObs.subscribe({
     //   next(msgs) {
@@ -59,13 +31,5 @@ export class ChatComponent implements OnInit {
     //   },
     // });
     // this.messagesRef.push({ content: 'bye' });
-  }
-
-  public sendMessage(inp: string) {
-    this.messagesRef.push({
-      content: inp,
-      sender: this.senderName,
-      senderId: this.senderUid
-    })
   }
 }
