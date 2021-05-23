5003377d875ca231f560c02521cb2c8092a6bef9
Samyak S Sarnayak <samyak201@gmail.com>
2021-05-23 14:00:40 +0530

    one emp moves to exit

--- misc/auto-demo/ws-bot.py
+++ misc/auto-demo/ws-bot.py
@@ -1,19 +1,20 @@
-import random
 import asyncio
-import websockets
 import json
+import random
+from pprint import pprint
 
 import numpy as np
+import requests
+import websockets
 
 uri = "ws://35.244.26.150:5050/update?id="
+apiUri = "http://35.244.26.150:8000"
+mapId = "2ef08d0b-dc55-490f-9b98-4f33cb85a254"
+scale = 5.380952380952381
 
 
 async def send1(name):
-    pos1 = {
-        "name": "Samyak",
-        "x": 315,
-        "y": 87
-    }
+    pos1 = {"name": "Samyak", "x": 315, "y": 87}
 
     async with websockets.connect(uri + name) as websocket:
         while True:
@@ -25,11 +26,7 @@ async def send1(name):
 
 
 async def send3(name):
-    pos1 = {
-        "name": "Aayush",
-        "x": 546,
-        "y": 65
-    }
+    pos1 = {"name": "Aayush", "x": 546, "y": 65}
 
     async with websockets.connect(uri + name) as websocket:
         while True:
@@ -41,15 +38,11 @@ async def send3(name):
 
 
 async def send2(name):
-    pos1 = {
-        "name": "Emp 2",
-        "x": 365,
-        "y": 100
-    }
+    pos1 = {"name": "Emp 2", "x": 365, "y": 100}
 
     async with websockets.connect(uri + name) as websocket:
-        width = 796
-        height = 415
+        # width = 796
+        # height = 415
 
         part1speed = 30
         part1x = np.linspace(87, 231, part1speed)
@@ -80,8 +73,46 @@ async def send2(name):
             await send(x, y)
 
 
+def _post_nearest_exit_path(initial_pos):
+    r = requests.post(
+        f"{apiUri}/map/nearestExit",
+        params={"id_": mapId},
+        json={"x": initial_pos["x"] / scale, "y": initial_pos["y"] / scale},
+    )
+
+    resp = r.json()
+    # pprint(resp)
+
+    if "path" in resp:
+        path = resp["path"]
+        path = [[x * scale - 10, y * scale - 10] for (x, y) in path]
+        return path
+    else:
+        return []
+
+
+async def emp_exit(uid, initial_pos):
+    path = _post_nearest_exit_path(initial_pos)
+
+    pprint(path)
+    # print(r.json())
+    pos = initial_pos
+
+    async with websockets.connect(uri + uid) as websocket:
+        async def send(x, y, pause=0.5):
+            pos["x"] = x
+            pos["y"] = y
+            print(f"sending for uid {uid}: ", pos)
+            await websocket.send(json.dumps(pos))
+            await asyncio.sleep(pause)
+
+        for x, y in path:
+            await send(x, y, 0.5)
+
+
 async def main():
-    await asyncio.gather(send1("brr"), send2("abc"), send3("592s1XmfNwYujg7Y1thbkDyOTZf2"))
+    # await asyncio.gather(send1("brr"), send2("abc"), send3("592s1XmfNwYujg7Y1thbkDyOTZf2"))
+    await asyncio.gather(emp_exit("emp1", {"name": "EMP 1", "x": 344, "y": 110}))
 
 
 asyncio.run(main())
