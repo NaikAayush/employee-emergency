bfec84c5605e60c84b0e5ec9c97be6912f022e3e
NaikAayush <ayush@pesu.pes.edu>
2021-05-24 11:24:11 +0530

    latest apk

--- orange-ecc/src/app/app-routing.module.ts
+++ orange-ecc/src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'employee',
+    redirectTo: 'home',
     pathMatch: 'full',
   },
   {
